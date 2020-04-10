bfef6237b7a88e229b18938f3b96d7cc920b3558
Mahfuzur Rahman <mahfuzurrahman.dev@gmail.com>
2020-04-11 00:57:07 +0600

    null vs undefined is done

---
+++ null-vs-undefined.js
@@ -0,0 +1,24 @@
+//var undefined 
+    let name;
+    console.log(name);
+
+//function undefined
+    function add(num1,num2){
+        console.log(num1 + num2);
+    }
+    const result = add(10,12);
+    // const result = add(10);
+    console.log(result);
+
+//property doesn't exist in a object
+    const school ={name:"SGMSC", place:"TB Gate"};
+    console.log(school.phoneNumber);
+    
+//undefined keyword
+    const life = undefined;
+    console.log(life);
+
+//Variable declared and assigned to null  
+    var a = null;   
+    console.log( a );   //output: null  
+    console.log( typeof(a) );   //output: object  
